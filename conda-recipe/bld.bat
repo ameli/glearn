540fd96e2c07837c69cfab621553a42094f0511e
Siavash Ameli <sia.sunrise@gmail.com>
2021-11-25 14:49:14 -0800

    conda13

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -9,4 +9,61 @@ rem directory of this source tree.
 @echo off
 setlocal EnableDelayedExpansion
 
-python -m pip install . -vv --no-binary :all:
+rem Get Python major and minor version
+for /f %%i in ('python -c "import sys; print(sys.version_info.major)"') do set python_version_major=%%i
+for /f %%i in ('python -c "import sys; print(sys.version_info.minor)"') do set python_version_minor=%%i
+
+rem Concatenate major and minor versions
+set python_version="cp%python_version_major%%python_version_minor%"
+
+rem Platform
+set platform="win"
+
+rem Directory at which wheel files are located
+set root_dir=%CD%\dist
+
+rem flag if any of the wheels are installed
+set wheel_installed=false
+
+rem Iterate through all wheel files in /dist/* directory.
+for /R %root_dir% %%i in (*.whl) do (
+    
+    rem Check if wheel filename matches python version
+    	echo %%i | findstr /C:"%python_version%">nul && (
+	    	set python_version_matched=true
+	) || (
+		set python_version_matched=false
+	)
+
+	rem Check if wheel filename matches platform
+    	echo %%i | findstr /C:"%platform%">nul && (
+	    	set platform_matched=true
+	) || (
+		set platform_matched=false
+	)
+
+	rem Install wheel if python version and platform matched
+	if "!wheel_installed!"=="false" (
+		if "!python_version_matched!"=="true" (
+			if "!platform_matched!"=="true" (
+
+				echo Try installing %%i for python version %python_version%
+                python -m pip install --force-reinstall %%i --verbose
+
+				rem Check last error
+				if errorlevel==0 (
+					set wheel_installed=true
+					echo Wheel installed successfully.
+					goto :break
+				)
+			)
+		)
+	)
+)
+
+rem Check if any of the wheels in \dist\* could be installed
+:break
+if "!wheel_installed!"=="false" (
+	echo No wheel could be installed.
+	exit /b 1
+)
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -11,4 +11,47 @@
 set -e
 set -x
 
-python -m pip install . -vv --no-binary :all:
+# Get python version
+python_version_major=`python -c 'import sys; print(sys.version_info.major)'`
+python_version_minor=`python -c 'import sys; print(sys.version_info.minor)'`
+python_version="cp${python_version_major}${python_version_minor}"
+
+# Detect operating system
+os=`uname`;
+case "$os" in
+    *"Linux"*) platform="linux";;
+    *"Darwin"*) platform="macosx";;
+    *) echo "Invalid operating system. os: $os."; exit 1;;
+esac
+
+# Iterate through all wheel files in /dist/* directory.
+wheel_installed=false;
+for wheel_filename in dist/*.whl;
+do
+    # Check if wheel filename matches python version and platform
+    if echo $wheel_filename | grep $python_version | grep $platform; then
+
+        # Matched. Install with pip
+        echo "Try installing wheel file '${wheel_filename}'.";
+        python -m pip install --no-deps --force-reinstall $wheel_filename --verbose;
+
+        # Check last error code
+        error_code=$?;
+        if [ "${error_code}" -eq 0 ];
+        then
+            echo "Wheel '${wheel_filename}' installed successfully.";
+            wheel_installed=true;
+            break;
+        fi
+    else
+        # Did not match. Skip installing this wheel file.
+        echo -n "Wheel file '${wheel_filename}' didn't match python version: ";
+        echo "'${python_version}' and platform: '${platform}'. Skipping.";
+    fi
+done
+
+if [ "$wheel_installed" != true ];
+then
+    echo "ERROR: No wheel could be installed." 1>&2;
+    exit 1;
+fi
